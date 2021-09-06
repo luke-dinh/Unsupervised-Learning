07342b38941f6035a70630eb65414cf9335df3ec
luke-dinh <dinhphuoclockt@gmail.com>
2021-09-06 15:26:17 +0700

    changes

---
+++ 03-AutoEncoder/train/train_ae.py
@@ -0,0 +1,43 @@
+import torch
+import numpy as np
+from torchvision import datasets
+from torchvision.utils import make_grid
+from torch.utils.data import dataloader
+import torchvision.transforms as transforms 
+import torch.nn.functional as F
+from model import ae
+
+def loss_fn(output_x, x):
+    return F.mse_loss(output_x, x)
+
+
+class ImproveChecker():
+	def __init__(self, mode='min', best_val=None):
+		assert mode in ['min', 'max']
+		self.mode = mode
+		if best_val is not None:
+			self.best_val = best_val
+		else:
+			if self.mode=='min':
+				self.best_val = np.inf
+			elif self.mode=='max':
+				self.best_val = 0.0
+
+	def check(self, val):
+		if self.mode=='min':
+			if val < self.best_val:
+				print("[%s] Improved from %.4f to %.4f" % (self.__class__.__name__, self.best_val, val))
+				self.best_val = val
+				return True
+			else:
+				print("[%s] Not improved from %.4f" % (self.__class__.__name__, self.best_val))
+				return False
+		else:
+			if val > self.best_val:
+				print("[%s] Improved from %.4f to %.4f" % (self.__class__.__name__, self.best_val, val))
+				self.best_val = val
+				return True
+			else:
+				print("[%s] Not improved from %.4f" % (self.__class__.__name__, self.best_val))
+				return False
+
