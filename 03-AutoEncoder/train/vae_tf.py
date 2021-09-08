5ee3d7781f8e863226ad55dae1de1a241bc7d4ee
luke-dinh <dinhphuoclockt@gmail.com>
2021-09-08 10:54:11 +0700

    update

--- 03-AutoEncoder/train/vae_tf.py
+++ 03-AutoEncoder/train/vae_tf.py
@@ -9,7 +9,7 @@ latent_dims = 2
 inputs = keras.Input(shape=(784,))
 
 # Encoder block
-encoder = encoder = Dense(512, activation='relu')(inputs)
+encoder = Dense(512, activation='relu')(inputs)
 encoder = Dense(256, activation='relu')(encoder)
 encoder = Dense(128, activation='relu')(encoder)
 h = Dense(encoding_dim, activation='relu')(encoder)
@@ -18,3 +18,16 @@ z_mean = Dense(latent_dims)(h)
 z_log_sigma = Dense(latent_dims)(h)
 
 # Sampling block
+
+def sampling(args):
+
+    z_mean, z_log_sigma = args 
+
+    epsilon = K.random_normal(shape=(K.shape(z_mean)[0], latent_dims), 
+                                mean=0, stddev=0.1)
+
+    return z_mean + K.exp(z_log_sigma) * epsilon
+
+z = keras.layers.Lambda(sampling)([z_mean, z_log_sigma])
+encoder = keras.Model(inputs, [z_mean, z_log_sigma, z], name='encoder')
+
