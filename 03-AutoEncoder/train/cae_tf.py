ee427b2eb565e0653239958de3b3d4d9cdc92b61
luke-dinh <dinhphuoclockt@gmail.com>
2021-09-13 14:23:51 +0700

    update

--- 03-AutoEncoder/train/cae_tf.py
+++ 03-AutoEncoder/train/cae_tf.py
@@ -1,5 +1,5 @@
 import keras
-from keras.layers import Conv2D, UpSampling2D, LeakyReLU, MaxPool2D
+from keras.layers import Conv2D, Conv2DTranspose, LeakyReLU, MaxPool2D
 from keras.datasets import cifar100
 
 # Define model
@@ -10,7 +10,14 @@ input_image = keras.Input(shape=(32,32,3))
 x = Conv2D(16, (3,3))(input_image)
 x = LeakyReLU(alpha=0.1)(x)
 x = MaxPool2D(pool_size=(2,2))(x)
-x = Conv2D(32, (3,3))(x)
+x = Conv2D(16, (3,3))(x)
 x = LeakyReLU(alpha=0.1)(x)
 encode = MaxPool2D(pool_size=(2,2))(x)
+
+# Decoder
+x = Conv2DTranspose(16, (3,3))(encode)
+x = LeakyReLU(alpha=0.1)(x)
+x = Conv2DTranspose(16, (3,3))(x)
+x = LeakyReLU(alpha=0.1)(x)
+decode = Conv2D(1, (3,3), activation='sigmoid')(x)
 
