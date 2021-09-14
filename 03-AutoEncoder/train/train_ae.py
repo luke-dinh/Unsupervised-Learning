79e71b63ce86f27184129ffc46df1e0bdc3aeb9a
luke-dinh <dinhphuoclockt@gmail.com>
2021-09-14 15:06:02 +0700

    changes

--- 03-AutoEncoder/train/train_ae.py
+++ 03-AutoEncoder/train/train_ae.py
@@ -7,60 +7,60 @@ from torchvision import datasets
 from torch.utils.data import dataloader
 import torchvision.transforms as transforms 
 import torch.nn.functional as F
-from model import ae 
+# from model import ae 
 import os 
 
 def loss_fn(output_x, x):
     return F.mse_loss(output_x, x)
 
 # In case this module cannot load the model from model:
-# class ae(nn.Module):
-#     def __init__(self, in_dims=784, encod_dims=64, negative_slope=0.1):
+class ae(nn.Module):
+    def __init__(self, in_dims=784, encod_dims=64, negative_slope=0.1):
 
-#         super(ae, self).__init__()
+        super(ae, self).__init__()
 
-#         # Encoder
-#         self.encoder = nn.Sequential(OrderedDict([ 
-#             ('layer1', nn.Linear(in_dims, 512)),
-#             ('relu1', nn.LeakyReLU(negative_slope=negative_slope, inplace=True)),
-#             ('layer2', nn.Linear(512, 256)),
-#             ('relu2', nn.LeakyReLU(negative_slope, inplace=True)),
-#             ('layer3', nn.Linear(256, 128)),
-#             ('relu3', nn.LeakyReLU(negative_slope, inplace=True)),
-#             ('layer4', nn.Linear(128, encod_dims)),
-#             ('relu4', nn.LeakyReLU(negative_slope, inplace=True)),
-#         ]))
+        # Encoder
+        self.encoder = nn.Sequential(OrderedDict([ 
+            ('layer1', nn.Linear(in_dims, 512)),
+            ('relu1', nn.LeakyReLU(negative_slope=negative_slope, inplace=True)),
+            ('layer2', nn.Linear(512, 256)),
+            ('relu2', nn.LeakyReLU(negative_slope, inplace=True)),
+            ('layer3', nn.Linear(256, 128)),
+            ('relu3', nn.LeakyReLU(negative_slope, inplace=True)),
+            ('layer4', nn.Linear(128, encod_dims)),
+            ('relu4', nn.LeakyReLU(negative_slope, inplace=True)),
+        ]))
 
-#         # Decoder
-#         self.decoder = nn.Sequential(OrderedDict([ 
-#             ('layer1', nn.Linear(encod_dims, 128)),
-#             ('relu1', nn.LeakyReLU(negative_slope, inplace=True)),
-#             ('layer2', nn.Linear(128, 256)),
-#             ('relu2', nn.LeakyReLU(negative_slope, inplace=True)),
-#             ('layer3', nn.Linear(256, 512)),
-#             ('relu3', nn.LeakyReLU(negative_slope, inplace=True)),
-#             ('layer4', nn.Linear(512, in_dims)),
-#             ('sigmoid', nn.Sigmoid()),
-#         ]))
+        # Decoder
+        self.decoder = nn.Sequential(OrderedDict([ 
+            ('layer1', nn.Linear(encod_dims, 128)),
+            ('relu1', nn.LeakyReLU(negative_slope, inplace=True)),
+            ('layer2', nn.Linear(128, 256)),
+            ('relu2', nn.LeakyReLU(negative_slope, inplace=True)),
+            ('layer3', nn.Linear(256, 512)),
+            ('relu3', nn.LeakyReLU(negative_slope, inplace=True)),
+            ('layer4', nn.Linear(512, in_dims)),
+            ('sigmoid', nn.Sigmoid()),
+        ]))
 
-#         self._init_weights()
+        self._init_weights()
 
-#     def forward(self, x):
+    def forward(self, x):
 
-#         z = self.encoder(x)
-#         out = self.decoder(z)
+        z = self.encoder(x)
+        out = self.decoder(z)
 
-#         return out
+        return out
 
-#     def _init_weights(self):
+    def _init_weights(self):
 
-#         for m in self.modules():
-#             if isinstance(m, nn.Linear):
-#                 m.weight.data.normal_(0, 0.01)
-#                 m.bias.data.zero_()
-#             elif isinstance(m, nn.BatchNorm2d):
-#                 m.weight.data.fill_(1)
-#                 m.bias.data.zero_()
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                m.weight.data.normal_(0, 0.01)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
 
 class ImproveChecker():
 	def __init__(self, mode='min', best_val=None):
@@ -93,7 +93,7 @@ class ImproveChecker():
 				return False
 
 # Load model
-model = ae.ae(in_dims=784, encod_dims=64)
+model = ae(in_dims=784, encod_dims=64)
 
 # Or using class ae in this module:
 # model = ae(in_dims=784, encod_dims=64)
@@ -141,6 +141,6 @@ for epoch in range(1, 50):
 			state_dict=model.state_dict(),
 			optimizer=optimizer.state_dict(),
 		)
-		save_file = os.path.join(new_path, "/ae.pth")
+		save_file = os.path.join(new_path, "ae.pth")
 		torch.save(checkpoint, save_file)
 		print("Best checkpoint is saved at %s" % (save_file))
