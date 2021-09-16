4a763d93649a3dd05114bacb11cf22e3e56b866e
luke-dinh <dinhphuoclockt@gmail.com>
2021-09-16 11:22:58 +0700

    update

--- 03-AutoEncoder/train/train_cae.py
+++ 03-AutoEncoder/train/train_cae.py
@@ -32,3 +32,33 @@ class cae(nn.Module):
             # ('relu2', nn.LeakyReLU(negative_slope, inplace=True)),
             ('sigmoid', nn.Sigmoid()),
         ]))
+
+class ImproveChecker():
+	def __init__(self, mode='min', best_val=None):
+		assert mode in ['min', 'max']
+		self.mode = mode
+		if best_val is not None:
+			self.best_val = best_val
+		else:
+			if self.mode=='min':
+				self.best_val = np.inf
+			elif self.mode=='max':
+				self.best_val = 0.0
+
+	def check(self, val):
+		if self.mode=='min':
+			if val < self.best_val:
+				print("[%s] Improved from %.4f to %.4f" % (self.__class__.__name__, self.best_val, val))
+				self.best_val = val
+				return True
+			else:
+				print("[%s] Not improved from %.4f" % (self.__class__.__name__, self.best_val))
+				return False
+		else:
+			if val > self.best_val:
+				print("[%s] Improved from %.4f to %.4f" % (self.__class__.__name__, self.best_val, val))
+				self.best_val = val
+				return True
+			else:
+				print("[%s] Not improved from %.4f" % (self.__class__.__name__, self.best_val))
+				return False
