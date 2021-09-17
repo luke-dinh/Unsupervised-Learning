a361ac37960bd6a5e18f6baa346de8db6665caa3
luke-dinh <dinhphuoclockt@gmail.com>
2021-09-17 16:28:08 +0700

    Update

--- 03-AutoEncoder/train/train_cae_2.py
+++ 03-AutoEncoder/train/train_cae_2.py
@@ -1,10 +1,11 @@
 import torch
 from torch.utils.data import DataLoader
 import numpy as np
-from torch.utils.data import sampler
+from torch import nn 
 from torchvision.datasets import MNIST
 import torchvision.transforms as transforms
 from torch.utils.data.sampler import SubsetRandomSampler
+
 
 
 def dataset(batch_size):
@@ -45,3 +46,29 @@ def dataset(batch_size):
     test_loader = DataLoader(test_data, batch_size=batch_size, num_workers=0)
 
     return train_loader, val_loader, test_loader
+
+# Define model
+
+class cae(torch.nn.Module):
+
+    def __init__(self, negative_slope=0.1):
+
+        super(cae, self).__init__()
+
+        # Encoder
+        self.encoder= nn.Sequential(OrderedDict([ 
+            ('layer1', nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, padding=1)),
+            ('relu1', nn.LeakyReLU(negative_slope, inplace=True)),
+            ('pooling1', nn.MaxPool2d(kernel_size=2)),
+            ('layer2', nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1)),
+            ('relu2', nn.LeakyReLU(negative_slope, inplace=True)),
+            ('pooling2', nn.MaxPool2d(kernel_size=2)),
+        ]))
+
+        # Decoder
+        self.decoder = nn.Sequential(OrderedDict([ 
+            ('layer1', nn.ConvTranspose2d(32, 16, 4, stride=2)),
+            ('relu1', nn.LeakyReLU(negative_slope, inplace=True)),
+            ('layer2', nn.ConvTranspose2d(16, 3, 4, stride=2)),
+            ('sigmoid', nn.Sigmoid()),
+        ]))
