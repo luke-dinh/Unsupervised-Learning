82b2fc5fbf1155141d613b179dfc4177dc9dbd95
luke-dinh <dinhphuoclockt@gmail.com>
2021-10-05 22:00:33 +0700

    Update

--- 03-AutoEncoder/eval/eval_vae.py
+++ 03-AutoEncoder/eval/eval_vae.py
@@ -4,5 +4,8 @@ from torchvision.utils import make_grid
 import torchvision.transforms as transforms
 
 import matplotlib.pyplot as plt
-import sys 
+import sys
+from model.vae import vae 
 
+main_path = "/home/lukedinh/Desktop/Unsupervised-Learning/03-AutoEncoder"
+sys.path.append(main_path)
