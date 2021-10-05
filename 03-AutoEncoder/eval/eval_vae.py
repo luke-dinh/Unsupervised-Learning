1d1db446e5b2667f3ec36faff6a44027220af225
luke-dinh <dinhphuoclockt@gmail.com>
2021-10-05 21:58:30 +0700

    Update

---
+++ 03-AutoEncoder/eval/eval_vae.py
@@ -0,0 +1,8 @@
+import torch, os 
+from torchvision.datasets import MNIST
+from torchvision.utils import make_grid
+import torchvision.transforms as transforms
+
+import matplotlib.pyplot as plt
+import sys 
+
