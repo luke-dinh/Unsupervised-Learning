6a46a7aba9292289dccf91e79739f16cf16e06d0
luke-dinh <dinhphuoclockt@gmail.com>
2021-10-05 22:03:17 +0700

    Update

--- 03-AutoEncoder/eval/eval_vae.py
+++ 03-AutoEncoder/eval/eval_vae.py
@@ -7,5 +7,10 @@ import matplotlib.pyplot as plt
 import sys
 from model.vae import VAE
 
+# Change path
 main_path = "/home/lukedinh/Desktop/Unsupervised-Learning/03-AutoEncoder"
 sys.path.append(main_path)
+
+model = VAE(in_dims=784, encod_dims=64)
+model.load_state_dict(torch.load(main_path + '/checkpoint/vae.pth', map_location="cpu")['state_dict'])
+model.eval()
