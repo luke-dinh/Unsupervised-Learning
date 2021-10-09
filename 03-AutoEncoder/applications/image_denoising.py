503416567b89944c8a98f20cd30877c1b3447537
luke-dinh <dinhphuoclockt@gmail.com>
2021-10-09 22:31:07 +0700

    Update

--- 03-AutoEncoder/applications/image_denoising.py
+++ 03-AutoEncoder/applications/image_denoising.py
@@ -12,5 +12,11 @@ parser.add_argument(
 )
 opt = parser.parse_args()
 main_path = opt.main_path
+model_path = opt.model_path
 sys.path.append(main_path)
 
+# Load model
+from model.ae import AE
+model = AE(in_dims=784, encod_dims=64)
+model.load_state_dict(torch.load(main_path + model_path, map_location="cpu")["state_dict"])
+model.eval()
