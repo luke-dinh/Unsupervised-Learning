249ced6f3bafccd644dead2d7d139db76cfdb521
luke-dinh <dinhphuoclockt@gmail.com>
2021-10-09 22:26:34 +0700

    Update

---
+++ 03-AutoEncoder/applications/image_denoising.py
@@ -0,0 +1,13 @@
+import argparse
+import torch
+import sys
+
+parser = argparse.ArgumentParser("Denoising Images using AE")
+parser.add_argument("--model_path", default="/checkpoint/ae.pth", type=str, help="Path to model's weight")
+parser.add_argument( 
+    "--main_path",
+    default="/home/lukedinh/Desktop/Unsupervised-Learning/03-AutoEncoder",
+    type=str,
+    help="Path to your folder"
+)
+opt = parser.parse_args()
