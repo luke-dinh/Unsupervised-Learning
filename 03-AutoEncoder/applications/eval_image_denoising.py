d4487b4f3eec1b10ac5906af8fa063798c7afc6a
luke-dinh <dinhphuoclockt@gmail.com>
2021-10-11 21:44:14 +0700

    Update

--- 03-AutoEncoder/applications/eval_image_denoising.py
+++ 03-AutoEncoder/applications/eval_image_denoising.py
@@ -6,3 +6,14 @@ import torch
 from torchvision.datasets import MNIST
 from torch.utils.data import DataLoader 
 import torchvision.transforms as transforms
+
+parser = argparse.ArgumentParser("Image Denoising Evaluation")
+parser.add_argument( 
+    "--main_path",
+    default="/home/lukedinh/Desktop/Unsupervised-Learning/03-AutoEncoder",
+    type=str,
+    help="Your main path"
+)
+opt = parser.parse_args()
+main_path = opt.main_path
+sys.path.append(main_path)
