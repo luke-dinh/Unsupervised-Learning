bcca9e2cc263a6b16f4873abb78f024ebecfbcbe
luke-dinh <dinhphuoclockt@gmail.com>
2021-10-13 14:32:22 +0700

    Update

---
+++ 03-Association Rule /association_rules.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from mlxtend.preprocessing import TransactionEncoder
+
+data = [ 
+    ["Milk", "Egg", "Bread"],
+    ["Milk, Bread"],
+    ["Milk", "Eggs"],
+    ["Eggs", "Banana"],
+]
+
