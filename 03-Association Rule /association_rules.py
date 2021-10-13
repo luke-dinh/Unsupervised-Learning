124d1818e08fe0fb17ce842e7ead864b03d9efc3
luke-dinh <dinhphuoclockt@gmail.com>
2021-10-13 15:19:51 +0700

    Changes

--- 03-Association Rule /association_rules.py
+++ 03-Association Rule /association_rules.py
@@ -3,10 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import apyori
 
-data = [ 
-    ["Milk", "Egg", "Bread"],
-    ["Milk, Bread"],
-    ["Milk", "Eggs"],
-    ["Eggs", "Banana"],
-]
+# data = [ 
+#     ["Milk", "Egg", "Bread"],
+#     ["Milk, Bread"],
+#     ["Milk", "Eggs"],
+#     ["Eggs", "Banana"],
+# ]
 
+# import data
+store_data = pd.read_csv("Online Retail.xlsx", headre=None)
