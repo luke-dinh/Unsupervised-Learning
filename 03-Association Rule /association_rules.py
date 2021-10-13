bca037618f77d14965642157a33fbf1eb2d44f14
luke-dinh <dinhphuoclockt@gmail.com>
2021-10-13 15:33:36 +0700

    Update

--- 03-Association Rule /association_rules.py
+++ 03-Association Rule /association_rules.py
@@ -11,11 +11,32 @@ import apyori
 # ]
 
 # import data
-store_data = pd.read_csv("store_data.csv", headre=None)
+store_data = pd.read_csv("store_data.csv", header=None)
 
 # Preprocessing
 records = []
 for i in range(0, 7501):
     records.append([str(store_data.values[i,j]) for j in range(0,20)])
 
+# Applying apyori algorithm
+association_rules = apyori.apriori(records, min_support=0.0045, min_confidence=0.2, min_lift=3, min_length=2)
+association_results = list(association_rules)
 
+# Display the rules
+for item in association_rules:
+
+    # first index of the inner list
+    # Contains base item and add item
+    pair = item[0] 
+    items = [x for x in pair]
+    print("Rule: " + items[0] + " -> " + items[1])
+
+    #second index of the inner list
+    print("Support: " + str(item[1]))
+
+    #third index of the list located at 0th
+    #of the third index of the inner list
+
+    print("Confidence: " + str(item[2][0][2]))
+    print("Lift: " + str(item[2][0][3]))
+    print("=====================================")
