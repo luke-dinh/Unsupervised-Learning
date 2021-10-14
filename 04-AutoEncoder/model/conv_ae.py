f971d38fc9b6190f1ac6ff495d125515368e3fa6
luke-dinh <dinhphuoclockt@gmail.com>
2021-10-14 21:34:16 +0700

    Update

--- 04-AutoEncoder/model/conv_ae.py
+++ 04-AutoEncoder/model/conv_ae.py
@@ -2,17 +2,19 @@ from collections import OrderedDict
 
 import torch
 from torch import nn 
-import torch.nn.functional as F 
+import torch.nn.functional as F
+from torchvision.datasets import MNIST
+import torchvision.transforms as transforms
 
-class cae(nn.Module):
+class conv_ae(nn.Module):
 
     def __init__(self, negative_slope=0.1):
 
-        super(cae, self).__init__()
+        super(conv_ae, self).__init__()
 
         # Encoder
         self.encoder= nn.Sequential(OrderedDict([ 
-            ('layer1', nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, padding=1)),
+            ('layer1', nn.Conv2d(in_channels=1, out_channels=16, kernel_size=3, padding=1)),
             ('relu1', nn.LeakyReLU(negative_slope, inplace=True)),
             ('pooling1', nn.MaxPool2d(kernel_size=2)),
             ('layer2', nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1)),
@@ -24,7 +26,7 @@ class cae(nn.Module):
         self.decoder = nn.Sequential(OrderedDict([ 
             ('layer1', nn.ConvTranspose2d(32, 16, 4, stride=2)),
             ('relu1', nn.LeakyReLU(negative_slope, inplace=True)),
-            ('layer2', nn.ConvTranspose2d(16, 3, 4, stride=2)),
+            ('layer2', nn.ConvTranspose2d(16, 1, 4, stride=2)),
             ('sigmoid', nn.Sigmoid()),
         ]))
 
@@ -47,7 +49,18 @@ class cae(nn.Module):
 
 # Test 
 if __name__ == "__main__":
-    model = cae(negative_slope=0.1)
+    model = conv_ae(negative_slope=0.1)
     model.eval()
 
-    print(model)
+    test_data = MNIST(root='.', train=False, download=True, 
+                        transform=transforms.Compose([ 
+                            transforms.ToTensor(),
+                            transforms.Normalize((0.137, ), (0.229, ))
+                        ]))
+    test_loader = torch.utils.data.DataLoader(test_data, batch_size=64, num_workers=4, shuffle=True)
+
+    test_iter = iter(test_loader)
+    images, labels = test_iter.next()
+
+    outputs = model(images)
+    print(outputs.shape)
