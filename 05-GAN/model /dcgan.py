164cce9e4d4aa4c94705a25c1434c9eab7953ccf
luke-dinh <dinhphuoclockt@gmail.com>
2021-11-17 20:59:30 +0700

    Update

--- 05-GAN/model /dcgan.py
+++ 05-GAN/model /dcgan.py
@@ -97,3 +97,11 @@ class discriminator(nn.Module):
 
     def forward(self, x):
         return self.discriminator(x)
+
+def _init_weights(self, m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
