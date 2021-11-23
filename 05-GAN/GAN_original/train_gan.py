c071f9c96c08aa98bc6e813c190ed0288a9b1db1
luke-dinh <dinhphuoclockt@gmail.com>
2021-11-23 21:23:30 +0700

    Update

--- 05-GAN/GAN_original/train_gan.py
+++ 05-GAN/GAN_original/train_gan.py
@@ -13,7 +13,18 @@ parser.add_argument("--num_epochs", type=int, default=5, help="Number of epochs 
 parser.add_argument("--n_gpu", type=int, default=0, help="Number of GPUs for training")
 parser.add_argument("--num_channels", default=3, type=int, help="Number of channels")
 parser.add_argument("--input_dim", default=100, type=int, help="Number of dimensions of inputs")
-parser.add_argument("--feature_map", default=32, type=int, help="Size of feature maps")
 parser.add_argument("--negative_slope", type=float, default=0.01, help="Negative slope parameter for LeakyReLU")
 parser.add_argument("--lr", type=float, default=0.01, help="Learning rate for optimizer")
 parser.add_argument("--batch_size", type=int, default=128, help="Batch size")
+
+opt = parser.parse_args()
+
+num_channels = opt.num_channels
+input_dim = opt.input_dim
+n_gpu = opt.n_gpu
+neg_slope = opt.negative_slope
+lr = opt.lr
+batch_size = opt.batch_size
+main_path = opt.main_path
+num_epochs = opt.num_epochs
+n_gpu = opt.n_gpu
